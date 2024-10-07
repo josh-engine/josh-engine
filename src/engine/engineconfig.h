9f76f331aabd30648da2623488768b15447a323d
hotcocoancode <elee9@madison.k12.wi.us>
2024-10-07 16:07:35 -0400

    Version increment bc UI library

--- src/engine/engineconfig.h
+++ src/engine/engineconfig.h
@@ -5,7 +5,7 @@
 #ifndef JOSHENGINE_ENGINECONFIG_H
 #define JOSHENGINE_ENGINECONFIG_H
 
-#define ENGINE_VERSION_STRING "Pre-Alpha 4b"
+#define ENGINE_VERSION_STRING "Pre-Alpha 5a"
 
 //#define DEBUG_ENABLED // This is now set by CMake. You CAN define it here but please just switch CMAKE_BUILD_TYPE
 
